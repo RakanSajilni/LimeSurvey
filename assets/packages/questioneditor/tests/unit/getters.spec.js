07c585f61dd36c76c6ed101bfde1254812fdea0e
Olle Harstedt <olleharstedt@yahoo.com>
2020-05-06 16:20:06 +0200

    Dev: Add getter test for PHP array as object (question editor)

--- assets/packages/questioneditor/tests/unit/getters.spec.js
+++ assets/packages/questioneditor/tests/unit/getters.spec.js
@@ -2,7 +2,15 @@ import getters from '../../src/storage/getters'
 
 test('hasIndividualAnsweroptionCodes is true for empty answer options', () => {
     const state = {
-        currentQuestionAnswerOptions: []
+        currentQuestionAnswerOptions: {}
+    };
+    const result = getters.hasIndividualAnsweroptionCodes(state);
+    expect(result).toBe(true);
+});
+
+test('hasIndividualAnsweroptionCodes is true for empty answer options with scale id', () => {
+    const state = {
+        currentQuestionAnswerOptions: {"0": []}
     };
     const result = getters.hasIndividualAnsweroptionCodes(state);
     expect(result).toBe(true);
@@ -54,6 +62,30 @@ test('hasIndividualAnsweroptionCodes is true for three different answer options'
     const result = getters.hasIndividualAnsweroptionCodes(state);
     expect(result).toBe(true);
 });
+
+test('hasIndividualAnsweroptionCodes is true for three different answer options in object', () => {
+    // This works since hasIndividualAnsweroptionCodes converts object into array.
+    // Real bug is why PHP sends array as object with int indexes.
+    const state = {
+        currentQuestionAnswerOptions: {
+            // NB: 0 = scale id
+            "0": {
+                "0": {
+                    code: "SQ00",
+                },
+                "1": {
+                    code: "SQ01",
+                },
+                "2": {
+                    code: "SQ02",
+                }
+            }
+        }
+    };
+    const result = getters.hasIndividualAnsweroptionCodes(state);
+    expect(result).toBe(true);
+});
+
 
 test('hasIndividualAnsweroptionCodes is false for two same answer options', () => {
     const state = {
